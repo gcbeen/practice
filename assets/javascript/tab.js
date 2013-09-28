bae62448b99f8e3ea38d359843e6a13f1f8051c2
been <been@been-ThinkPad-E520.(none)>
2013-09-28 21:29:11 +0800

    modify js

--- assets/javascript/tab.js
+++ assets/javascript/tab.js
@@ -1,34 +1,16 @@
-function up_tab(ele) {
-	console.log('kkkkkkkkkkkkkkkk');
-	console.log(ele);
-	var upEle = document.getElementById('tab').getElementsByClassName('up')[0];
-	var blockEle = document.getElementById('tab').getElementsByClassName('block')[0];
+function up_tab(eve) {
+	var eve = eve || window.event;
+	console.log(eve);
+	var ele = eve.target || eve.srcElement;
+	var tabEle = document.getElementById('tab')
+	var upEle = tabEle.getElementsByClassName('up')[0];
+	var blockEle = tabEle.getElementsByClassName('block')[0];
 	upEle.className = upEle.className.replace('up', '');
 	blockEle.className = blockEle.className.replace('block', '');
 	ele.className += ' up';
-	console.log(ele.nextSibling);
-	ele.nextSibling.className += ' block';
+	console.log(ele.nextElementSibling);
+	ele.nextElementSibling.className += ' block';
 }
-function addClick() {
-	document.getElementById('tab').onclick = function (e) {
-		if (e.target == 'h3') {
-			up_tab(e.target);
-		}
-	};
-	/*
-	   var h3Eles = document.getElementById('tab').getElementsByTagName('h3');
-	   var i = h3Eles.length;
-	   while ( i ) {
-	   h3Eles[i-1].onclick = function () {
-	   console.log(h3Eles[i-1]);
-	   console.log(this);
-	   (function () {
-	   up_tab(h3Eles[i-1]);
-	   }());
-	   };
-	   i -= 1;
-	   }*/
-}
-window.onload = function () {
-	addClick();
-};
+document.getElementById('tab').addEventListener('click', function (event) {
+	up_tab(event);
+}, false);
